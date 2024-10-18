005e75fae40c5d5a1abcd40912044483c0a4da92
Shu Jing Xu <r0758834@kuleuven.be>
2024-10-18 20:42:02 +0200

    correcties aan oef 1.5

--- Week2/IR1.m
+++ Week2/IR1.m
@@ -56,7 +56,7 @@ out=simout.signals.values(:,1);
 
 %% Calculate the IR by trimming the output
 
-c = max(out);
+c = max(out); %impuls respons vinden
 x = 0;
 tol = 10^-9
 
@@ -81,48 +81,130 @@ plot(h);
 xlabel('Time [samples]')
 ylabel('Impulse response [arb.]')
 % Magnitude response
-
 subplot(2,1,2)
-plot(pow2db(abs(h).^2));
+plot((0:fs/length(h):fs-fs/length(h)), pow2db(abs(h).^2));
 xlabel('Frequency [Hz]')
 ylabel('Magnitude response [dB]')
 
 
 
-%% Filtered white noise Vs. recorded white noise.
-sig = wgn(N,1,0); % Generate white noise signal    
+
+%% 
+%{
+First, use IR1.m to make a new estimate of the IR. 
+
+
+Then,
+without moving the microphone, repeat the white-noise experiment from
+exercise 1-2. 
+
+
+If you now convolve the white noise signal with the estimated
+IR (use the command fftfilt), this should yield an output signal with
+similar characteristics as the recorded signal (why?). 
+
+%}
+
+
+% Filtered white noise Vs. recorded white noise.
+sig = wgn(N,1,0); % Generate white noise signal
+[simin,nbsecs,fs] = initparams(sig,fs,3);
+sim('recplay');
+unfiltered_noise= simout.signals.values(:,1);
+
+
+%filtered noise
+
+i = 1;
+max_signal = max(abs(unfiltered_noise))
+
+filtered_noise = unfiltered_noise;
+
+while i <= length(filtered_noise)
+    if(abs(filtered_noise(i)) >= 0.2*max_signal) %deze waarde moet eruit
+            filtered_noise(i) = 0;
+            i = i - 1;
+    end
+    i = i + 1;
+end
+
+
+figure; subplot(4,1,1)
+plot(unfiltered_noise)
+ylim([-0.08 0.08])
+ylabel('unfiltered noise')
+
+subplot(4,1,2)
+plot(filtered_noise)
+ylim([-0.08 0.08])
+ylabel('filtered noise')
+
+
+
 
 % Convolve the IR with the white noise
-gen_noise = fftfilt(h,sig);                  
+con_noise = fftfilt(h,filtered_noise); %IR convolueren met gefilterde ruis signaal
+%this should yield an output signal with similar characteristics as the recorded signal (why?).
+
+%dirac in frequentiedom is ook een dirac
 
 % Send the white noise across the channel
-[simin,nbsecs,fs] = initparams(gen_noise,fs,3);
+[simin,nbsecs,fs] = initparams(con_noise,fs,3);
 sim('recplay');
 rec_noise= simout.signals.values(:,1);
+
+subplot(4,1,3)
+plot(con_noise)
+ylabel('h*noise')
+
+subplot(4,1,4)
+plot(rec_noise)
+ylabel('recorded convolution: h*noise') %het is in tijd verschoven
+
+
+title('Noise')
 
 
 %% Spectrogram and PSD plot
 % Spectrogram
-spectrogram(sig,fs); % Generated white noise
-spectrogram(rec_noise,fs); % Recorded white noise
+
+%{
+Compare the spectrograms and PSDs of the recorded signal and the convolved signal. Do
+they look more or less the same? For the PSD estimation, you can use
+Welch’s method.
+%}
+
+
+%spectrograms
+figure; 
+title('spectogram')
+subplot(2,1,1)
+spectrogram(con_noise,N); % Generated white noise
+subplot(2,1,2)
+spectrogram(rec_noise,N); % Recorded white noise
 
 % PSD
-[s1,f1,t1] = spectrogram(sig,fs); % Generated white noise
-[s2,f2,t2] = spectrogram(rec_noise,fs); % Recorded white noise
+[s1,f1,t1] = spectrogram(sig,N); % Generated white noise
+[s2,f2,t2] = spectrogram(rec_noise,N); % Recorded white noise
 
 PSD_gen_noise = sum(abs(s1).^2,2)/T; % Generated white noise PSD
 PSD_rec_noise = sum(abs(s2).^2,2)/T; % Recorded white noise PSD (ideally only when the signal is active)
 
 figure;
-subplot(1,2,1)
+title('PSD')
+subplot(2,1,1)
 plot(f1,pow2db(PSD_gen_noise));
 xlabel('Frequency (kHz)');
 ylabel('Power/frequency (dB/Hz)')
-title('Generated white noise.')
+subtitle('Generated white noise.')
+
 % Output signal
-subplot(1,2,2)
+subplot(2,1,2)
 plot(f2,pow2db(PSD_rec_noise));
 xlabel('Frequency (kHz)');
 ylabel('Power/frequency (dB/Hz)')
-title('Output signal.')
-sgtitle('Recorded white noise')
+subtitle('Recorded white noise')
+
+
+
+
