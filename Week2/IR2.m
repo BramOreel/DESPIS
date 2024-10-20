483f9df36e40db5f46b0742445ece0ad6a1cbde1
shujing2000 <shuxu1@hotmail.be>
2024-10-20 03:35:49 +0200

    oef 2-2

--- Week2/IR2.m
+++ Week2/IR2.m
@@ -10,11 +10,11 @@ dftsize = 100; % Discrete Fourier Transform (DFT) size [Samples]
 % short-time-Fourier-transform (STFT) [samples]
 Noverlap = dftsize/2; 
 channelLength = 200; % Length of impulse response [samples]
-delay = 50; % Positive delay safety margin when aligning input and output [samples]
+delay = 200; % Positive delay safety margin when aligning input and output [samples]
 
 %% Create the signal to be played.
 duration = 1; % Duration of the signal in [s]
-N =duration*fs
+N = duration*fs;
 sig = wgn(N,1,0);  
 
 %% Filter signal -> only to be used for exercise 2.3 (To this end, also copy the content
@@ -30,8 +30,16 @@ sim('recplay');
 % Retrieve recorded output
 out=simout.signals.values(:,1);
 
+%figure; 
+%subplot(3,1,1)
+%plot(sig)
+
+%subplot(3,1,2)
+
+%plot(out)
+
 %% Calculate the impulse response.
-uMatrix = toeplitz(sig'); % Toeplitz matrix
+uMatrix = toeplitz(sig'); % Toeplitz matrix van de ruis
 
 
 %Find the max of the out signal
@@ -48,28 +56,14 @@ end
 
 yOnset = x; % Determine start of recorded signal [samples]
 
-y = out(yOnset-200:yOnset + N-201); % Extract the relevant output signal
+%y = out(yOnset-200:yOnset + N-201); % Extract the relevant output signal
+y = out(yOnset-delay:yOnset + N-delay-1);
+%subplot(3,1,3)
+%plot(y)
+%Causaal maken: shiften van out om h causaal te maken:
 
-h =  lsqr(uMatrix,y)       ; % Estimate impulse response
 
-
-%% Calculate the IR by trimming the output
-
-%{
-c2 = max(out);
-x2 = 0;
-tol = 10^-9
-
-for i = 1:size(out,1)
-    if(abs(out(i) -c2) < tol)
-        x2 = i;
-        break
-    end
-    
-end
-
-h = out(x2-25:x2+150);
-%}
+h =  lsqr(uMatrix,y,0.1)       ; % Estimate impulse response
 save('channel.mat','h'); % Save impulser response
 
 %% Plot IR.
@@ -78,48 +72,52 @@ figure; subplot(2,1,1)
 plot(h);
 xlabel('Time [samples]')
 ylabel('Impulse response [arb.]')
+
 % Magnitude response
 subplot(2,1,2)
-plot(pow2db(abs(h).^2));
+dfth = fft(h);
+plot(pow2db(abs(dfth).^2)); %anders omdat hier fft wordt gebruikt?
 xlabel('Frequency [Hz]')
 ylabel('Magnitude response [dB]')
 
-%{
 
 %% Filtered white noise Vs. recorded white noise.
-sig = wgn(); % Generate white noise signal    
+sig = wgn(N,1,0); % Generate white noise signal    
 
 % Convolve the IR with the white noise
-gen_noise = fftfilt();                  
+gen_noise = fftfilt(h,sig);                  
 
 % Send the white noise across the channel
-[] = initparams();
+[simin,nbsecs,fs] = initparams(sig,fs,3);
 sim('recplay');
 rec_noise=simout.signals.values(:,1);
 
 %% Spectrogram and PSD plot
 % Spectrogram
-spectrogram(); % Generated white noise
-spectrogram(); % Recorded white noise
+figure; 
+title('spectogram')
+subplot(2,1,1)
+spectrogram(sig,N); % Generated white noise
+subplot(2,1,2)
+spectrogram(rec_noise,N); % Recorded white noise
 
 % PSD
-[] = spectrogram(); % Generated white noise
-[] = spectrogram(); % Recorded white noise
+[s1,f1,t1] = spectrogram(sig,N); % Generated white noise
+[s2,f2,t2] = spectrogram(rec_noise,N); % Recorded white noise
 
-PSD_gen_noise = ; % Generated white noise PSD
-PSD_rec_noise = ; % Recorded white noise PSD (ideally only when the signal is active)
+PSD_gen_noise = sum(abs(s1).^2,2)*fs; % Generated white noise PSD
+PSD_rec_noise = sum(abs(s2).^2,2)*fs; % Recorded white noise PSD (ideally only when the signal is active)
 
 figure;
-plot(,pow2db());
+subplot(2,1,1)
+plot(f1,pow2db(PSD_gen_noise));
 xlabel('Frequency (kHz)');
 ylabel('Power/frequency (dB/Hz)')
 title('Generated white noise.')
 % Output signal
-subplot(2,2)
-plot(,pow2db());
+subplot(2,1,2)
+plot(f2,pow2db(PSD_rec_noise));
 xlabel('Frequency (kHz)');
 ylabel('Power/frequency (dB/Hz)')
 title('Output signal.')
 sgtitle('Recorded white noise')
-
-%}
