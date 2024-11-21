d31c4a4e8579072a0cb6ac60ce41dfe0614c6ed5
bramo <bram.oreel@student.kuleuven.be>
2024-11-21 22:15:59 +0100

    6.1.1 afgewerkt i guess

--- Session6/alignIO.m
+++ Session6/alignIO.m
@@ -9,15 +9,27 @@ function [ out_aligned ] = alignIO( out, fs )
 % out_aligned   T2X1    Aligned output of T2 samples.
 
 %% Define syncronization pulse
-sync_pulse = ;
+
+
+    T = 1/fs;
+    f0 =  100;
+    f1 = 2000;
+    duration = 1; %Duration of the pulse
+    t = 0:T:duration- T;
+    pulse = chirp(t,f0,duration,f1)'; %1kHz sin
+    sync_pulse = [pulse; zeros(fs,1)];
+
 
 %% Align I/O
 safety_margin = 50; % Safety margin [samples]
 
-[] = xcorr(); % Find cross-correlation between output and synchronisation pulse
-[~,maxIdx] = max(); % Find index of maximum cross-correlation
-delay = ; % Find delay corresponding to that lag of maximum cross-correlation
-startIdx = ; % Find start index
-out_aligned = out(); % Align output
+[correlation,lags] = xcorr(out,sync_pulse,'none'); % Find cross-correlation between output and synchronisation pulse
+[~,maxIdx] = max(abs(correlation)); % Find index of maximum cross-correlation
+delay = lags(maxIdx); % Find delay corresponding to that lag of maximum cross-correlation
+
+startIdx = delay + length(sync_pulse) - safety_margin; % Find start index
+out_aligned = out(startIdx:end);
+%plot(out_aligned);
+
 end
 
