128581a498f0ca9a7cff70b9b3db0c95ffba7937
bramo <bram.oreel@student.kuleuven.be>
2024-12-05 12:08:36 +0100

    7.1 klaar denkik

--- Session7/DDequalization.m
+++ Session7/DDequalization.m
@@ -10,12 +10,12 @@ clc; clear; close all;
 
 
 %% Parameters.
-M =16; % QAM constelllation size
+M =4; % QAM constelllation size
 Nq = log2(M);
 constellation = (-Nq+1:2:Nq-1) + 1j*(-Nq+1:2:Nq-1)';
 nbQAMsymb =0; % Number of QAM symbols 
 Hk = -1.3 + 5i; % Channel to consider (Only one frequency bin will be considered here)
-alpha = 10^-3; % Regularisation constant
+alpha = 1; % Regularisation constant
 SNR = 30; % Signal-to-noise-ratio [dB]
 
 N_filter = 1; %number of tones in the filter
@@ -37,7 +37,7 @@ iN = 1; % Counter of stepsizes
 
 %% Try to estimate wk as 1/Hk
 
-for mu = [0.2] % List of stepsizes
+for mu = [0.02 0.1 0.2 0.5 1 5 10] % List of stepsizes
     % NMLS filter implementation.
     % Initialise filters, reconstructed transmitted signal and error
     delta = 0.2;
@@ -47,13 +47,16 @@ for mu = [0.2] % List of stepsizes
     w(1) = (1 + delta)/conj(Hk); 
     for n = 1:length(Xk)-1
         % Apply filter.
-        estXk = w(n)'*Yk_noise(n);
+        estXk = conj(w(n))*Yk_noise(n);
         % Reconstruct transmitted signal.
         rec_Xk(n) = qammod(qamdemod(estXk,M),M);
         % Calculate error signal.
         Ek(n) =  Xk(n) - rec_Xk(n) ;
         % Update filter.
         w(n+1) = w(n) + mu/(alpha + conj(Yk_noise(n))*Yk_noise(n))*Yk_noise(n)*conj((Xk(n) - conj(w(n))*Yk_noise(n)));
+        if(abs(w(n+1)) > 10^30)
+            break
+        end
     end
      
     legendCell{iN} = num2str(mu,'mu=% .2f');
